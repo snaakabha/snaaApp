2c6a1037ad45fd4a16bfc76d34f5424d0e774730
snaakabha <157304365+snaakabha@users.noreply.github.com>
2024-01-28 18:08:49 +0200

    color
    color of text and the color of backgroundColor

--- general/LoginPage.jsx
+++ general/LoginPage.jsx
@@ -3,15 +3,11 @@ import React from 'react'
 
 
 const styles = StyleSheet.create({
-  app:{
-      flex: 1,
-      backgroundColor: "rgb(255,255,255)",
-  },
   container: {
     borderWidth: 1,
     borderColor: "black",
     alignSelf: 'center', padding: 12,
-    marginTop: 300,
+    marginTop: 300
   },
   RedBox: {
     backgroundColor: "red",
@@ -20,32 +16,24 @@ const styles = StyleSheet.create({
   flexBox: {
     flexDirection: "row",
     gap: 15
-    
-  
   },
   footer: {
     alignSelf: 'center'
-  }, 
-
- 
+  }
 })
 const App = () => {
   return (
-    <View style={styles.app}>
-
     <View style={styles.container}>
       <View style={styles.flexBox}>
-        <View >
-          <Text style={[{ color: "black" }]}>name: Snaa Medlij</Text>
-          <Text style={[{ color: "black" }]} >Bd: 24-01-2001</Text>
+        <View>
+          <Text>name: Snaa Medlij</Text>
+          <Text>Bd: 24-01-2001</Text>
         </View>
         <View style={styles.RedBox}></View>
       </View>
       <View style={styles.footer}> 
-        <Text style={[{ color: "black" }]} >My First UI</Text>
+        <Text>My First UI</Text>
       </View>
-    </View>
-
     </View>
   )
 }
